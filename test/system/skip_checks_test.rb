fdaef071974ca076bc047eda1e1a990096cb3b8b
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-08 00:17:22 +0300

    Fix: Add system test directory for CI

---
+++ test/system/skip_checks_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SkipChecksTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit skip_checks_url
+  #
+  #   assert_selector "h1", text: "SkipCheck"
+  # end
+end
