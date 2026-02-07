c6f7c2f8e3ee71ffc2634b354e1669dcf7ff93ae
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-08 00:09:47 +0300

    fix format

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,4 @@
 class ProjectsController < ApplicationController
-
     def index
         @projects = Project.all
     end
@@ -14,12 +13,12 @@ class ProjectsController < ApplicationController
 
     def create
         @project = Project.new(project_params)
-        if @project.save #is this a valid project?
-            flash[:notice] = "Project was created successfully!" #set a flash message to show on the next page
-            redirect_to project_path(@project) #redirect to show page for this new project
+        if @project.save # is this a valid project?
+            flash[:notice] = "Project was created successfully!" # set a flash message to show on the next page
+            redirect_to project_path(@project) # redirect to show page for this new project
         else
-            render :new , status: :unprocessable_entity #if not valid, re-render the new form with error messages
-        end 
+            render :new, status: :unprocessable_entity # if not valid, re-render the new form with error messages
+        end
     end
 
 
@@ -28,26 +27,25 @@ class ProjectsController < ApplicationController
     end
 
     def update
-        @project = Project.find(params[:id]);
+        @project = Project.find(params[:id])
         if @project.update(project_params)
             flash[:notice] = "Project was updated successfully!"
             redirect_to project_path(@project)
         else
-            render :edit , status: :unprocessable_entity
+            render :edit, status: :unprocessable_entity
         end
     end
 
 
     def project_params
-        params.expect(project: [:name])
+        params.expect(project: [ :name ])
     end
 
 
     def destroy
         @project = Project.find(params[:id])
-        @project.destroy 
-        flash[:notice] = "Project was deleted successfully!";
+        @project.destroy
+        flash[:notice] = "Project was deleted successfully!"
         redirect_to projects_path
     end
-    
 end
